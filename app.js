133084f6c4b7a2d9b8a76784a447267baad78c37
James Steed <james.steed23@student.cart.org>
2024-02-07 08:56:49 -0800

    finished DOM Demo

--- app.js
+++ app.js
@@ -9,24 +9,35 @@ document.addEventListener("DOMContentLoaded", function () {
 
     // optionally update when fields update
     // tbd
+    document.getElementById('name').addEventListener('change');
 
     // process form Data
     document.getElementById("myForm").addEventListener("submit", function (e) {
         e.preventDefault();
+
+        console.log(e.target);
       
-        var formData = new FormData(form);
+        var formData = new FormData(e.target);
+        formData = Object.fromEntries(formData);
+
         // output as an object
-        console.log(Object.fromEntries(formData));
+        // console.log(Object.fromEntries(formData));
       
         // ...or iterate through the name-value pairs
-        for (var pair of formData.entries()) {
-          console.log(pair[0] + ": " + pair[1]);
-        }
+        // for (var pair of formData.entries()) {
+        //   console.log(pair[0] + ": " + pair[1]);
+        // }
+
+        let nameElement = document.getElementById('nameTag');
+        let textElement = document.getElementById('info');
+
+        nameElement.innerText = formData.name;
+        textElement.innerText = formData.type;
       });
     // Log readiness to console
     console.log("Ready");
 });
 
 function processForm(form){
-
+  
 }
